bfdae70d9c990667c78c4b6e1214c4beab7c595d
dhairya <dh263020@sal.ca>
2022-11-16 19:36:23 -0400

    Added firebase

--- app/src/main/java/ca/dal/teacherly/utils/LoginManager.kt
+++ app/src/main/java/ca/dal/teacherly/utils/LoginManager.kt
@@ -1,4 +1,42 @@
 package ca.dal.teacherly.utils
 
-class LoginManager {
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import ca.dal.teacherly.R
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.login.*
+
+class LoginManager : AppCompatActivity() {
+    private lateinit var auth:FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+        auth = FirebaseAuth.getInstance()
+
+
+        LoginButton.setOnClickListener(){
+            if(checkForInput()){
+                val email = LoginEmail.text.toString()
+                val password = LoginPassword.text.toString()
+                auth.signInWithEmailAndPassword(email,password)
+                    .addOnCompleteListener(this){
+                        task->
+                        if(task.isSuccessful){
+                            Toast.makeText(this, "Login Successfull", Toast.LENGTH_LONG).show()
+                        }else{
+                            Toast.makeText(this, "Wrong Details", Toast.LENGTH_LONG).show()
+                        }
+                    }
+            }else{
+                Toast.makeText(this, "Enter the details", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun checkForInput(): Boolean {
+        if(LoginEmail.text.toString().trim().isNotEmpty() && LoginPassword.text.toString().trim().isNotEmpty())
+            return true
+        return false
+    }
 }
