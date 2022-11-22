60300bb4ddbcac56effb5286e73c593da0da62dd
Bharatwaaj Shankaranarayanan <bh277785@dal.ca>
2022-11-21 21:46:41 -0400

    Revert "Merge branch 'feat-tutor-dashboard' into 'main'"
    This reverts merge request !5

--- app/src/main/java/ca/dal/teacherly/utils/TeacherlyApplication.kt
+++ app/src/main/java/ca/dal/teacherly/utils/TeacherlyApplication.kt
@@ -1,28 +1,4 @@
 package ca.dal.teacherly.utils
 
-import android.app.Application
-
-class TeacherlyApplication : Application() {
-
-    var isTutor = false
-
-    override fun onCreate() {
-        super.onCreate()
-        this.isTutor = false
-        instance = this
-    }
-
-    companion object {
-        lateinit var instance: TeacherlyApplication
-        private set
-    }
-
-    fun setIsTutor(isTutor: Boolean){
-        this.isTutor = isTutor
-    }
-
-    fun getIsTutor() : Boolean{
-        return this.isTutor;
-    }
-
+class TeacherlyApplication {
 }
