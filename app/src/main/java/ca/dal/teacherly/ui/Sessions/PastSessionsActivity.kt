dee8d2a0d7f1e4ab356f2d84a3d30cc33a2fec56
Bharatwaaj Shankaranarayanan <bharatwaaj.1996@gmail.com>
2022-11-24 20:59:41 -0400

    Minor Refactoring on Name Changes

--- app/src/main/java/ca/dal/teacherly/ui/Sessions/PastSessionsActivity.kt
+++ app/src/main/java/ca/dal/teacherly/ui/Sessions/PastSessionsActivity.kt
@@ -3,17 +3,8 @@ package ca.dal.teacherly.ui.Sessions
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
 import ca.dal.teacherly.R
-import ca.dal.teacherly.adapters.SessionsAdapter
 import ca.dal.teacherly.controllers.SessionController
-import ca.dal.teacherly.data.InitialSessions
-import ca.dal.teacherly.models.Sessions
-import ca.dal.teacherly.utils.Constants
-import ca.dal.teacherly.utils.Utils
-import com.google.firebase.Timestamp
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_past_sessions.*
 
 /*
